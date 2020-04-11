bd17ce54aba7de771072d8506472e9af6bbb0f25
Ian Halstead <iwhalstead10@gmail.com>
2020-04-10 21:55:54 -0600

    Finished gallery.js for 2 images. Fixed some problems with css on google chrome

--- js/gallery.js
+++ js/gallery.js
@@ -1,103 +1,53 @@
-// const namesArr = [ 'test1.png', 'test2.png', 'Newsies_PRP_LOGO.png', 'Newsies_PRP_LOGO - Copy' ];
-// const imgElements = [];
+window.addEventListener('load', () => {
+	const gallery = document.getElementById('gallery');
+	const MARGIN = gallery.offsetWidth / 10;
+	const imagesHTML = gallery.getElementsByTagName('img');
+	let images = [].slice.call(imagesHTML);
+	images.sort((a, b) => a.width - b.width);
+	let min = 0;
+	let max = images.length - 1;
+	let closest = [];
 
-// for (let i in namesArr) {
-// 	let imgElement = document.createElement('img');
-// 	imgElement.setAttribute('src', 'img/' + namesArr[i]);
-// 	imgElements.push(new imageObject(imgElement.width, imgElement.height, imgElement));
-// }
+	const binarySearch = (value, arr, target, margin) => {
+		let mid = Math.floor((min + max) / 2);
 
-// // Sorts imgElements array based on the width of all elements
-// imgElements.sort((a, b) => a.width - b.width);
+		if (max < min) {
+			return -1;
+		}
+		else if (arr[mid].width + value < target - margin) {
+			min = mid + 1;
+			return binarySearch(value, arr, target, margin);
+		}
+		else if (arr[mid].width + value > target) {
+			max = mid - 1;
+			return binarySearch(value, arr, target, margin);
+		}
+		else {
+			return mid;
+		}
+	};
 
-// for (let i in imgElements) {
-// 	let min = 0;
-// 	let max = imgElements.length - 1;
-// 	let closest = [];
-// }
+	images.forEach((value, id) => {
+		images = images.slice(1);
+		console.log(value.width);
+		let target = gallery.offsetWidth - value.width;
 
-// const binarySearch = (arr, target, margin) => {
-// 	let mid = Math.floor((max + min) / 2);
+		min = 0;
+		max = images.length - 1;
+		closest = [];
+		let finalId = binarySearch(value.width, images, target, MARGIN);
 
-// 	if (arr[min].width > arr[max].width) {
-// 		return closest;
-// 	}
-// 	else if (arr[mid].width > target) {
-// 		max = mid - 1;
-// 		return binarySearch(arr, target, margin);
-// 	}
-// 	else if (arr[mid].width < target - margin) {
-// 		min = mid + 1;
-// 		closest.push(mid);
-// 		closest.sort((a, b) => imgElements[a].width - imgElements[b].width);
-// 		console.log(closest);
-// 		return binarySearch(arr, target, margin);
-// 	}
-// 	else {
-// 		return mid;
-// 	}
-// };
-
-// let min = 0;
-// let max = imgElements.length - 1;
-// let closest = [];
-// binarySearch(imgElements, 1000000, 0);
-
-// document.body.appendChild(imgElements[1].element);
-
-// // const binarySearch = (arr, target, margin) => {
-// // 	let mid = Math.floor((max + min) / 2);
-
-// // 	if (min > max) {
-// // 		return closest;
-// // 	}
-// // 	if (arr[mid].width > target) {
-// // 		max = mid - 1;
-// // 		return binarySearch(arr, target, margin);
-// // 	}
-// // 	else if (arr[mid].width < target - margin) {
-// // 		min = mid + 1;
-// // 		closest = mid;
-// // 		return binarySearch(arr, target, margin);
-// // 	}
-// // 	else {
-// // 		return mid;
-// // 	}
-// // };
-// while (localStorage.getItem('loaded') !== 1) {}
-
-const gallery = document.getElementById('gallery');
-const MARGIN = gallery.width / 10;
-const imagesHTML = gallery.getElementsByTagName('img');
-let images = [].slice.call(imagesHTML);
-images.sort((a, b) => a.width - b.width);
-
-let min = 0;
-let max = images.length - 1;
-let closest = [];
-const binarySearch = (arr, target, margin) => {
-	let mid = Math.floor((min + max) / 2);
-
-	if (max < min) {
-		return -1;
-	}
-	else if (arr[mid].width < target - margin) {
-		min = mid + 1;
-		return binarySearch(arr, target, margin);
-	}
-	else if (arr[mid].width > target) {
-		max = mid - 1;
-		return binarySearch(arr, target, margin);
-	}
-	else {
-		return mid;
-	}
-};
-
-// console.log(binarySearch(images, gallery.width, MARGIN));
-
-images.forEach((value, id) => {
-	images = images.slice(1);
-
-	let target = gallery.width - value.width;
+		if (finalId === -1) {
+			images.forEach((value) => closest.push(value));
+			console.log(closest);
+		}
+		else {
+			console.log('finalId: ', finalId);
+			console.log('Widths Added: ', value.width + images[finalId].width);
+			console.log('target :', target);
+			console.log('MARGIN :', MARGIN);
+		}
+	});
 });
+
+// ! THE PROGRAM FINALLY WOKS!!!!!! NOW YOU KNOW IF 2 IMGS WORK TOGETHER AND NEED TO GET IT WORKING FOR MORE THAN 2
