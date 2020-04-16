fa26ef1ba7957c77dfc034770599a0bd1e9f5fe1
Ian Halstead <iwhalstead10@gmail.com>
2020-04-16 17:33:27 -0600

    Hamburger animates but not every time

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -2,6 +2,14 @@ const nav = document.getElementById('navbar');
 const hamburger = document.getElementById('hamburger');
 
 hamburger.addEventListener('click', (e) => {
-	nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
+	if (nav.classList.contains('nav-anim')) {
+		nav.classList.remove('nav-anim');
+		nav.classList.add('nav-anim-hide');
+	}
+	else {
+		nav.classList.remove('nav-anim-hide');
+		nav.classList.add('nav-anim');
+	}
+
 	document.body.style.overflowY = 'hidden';
 });
