5f64afe9bd668049c98a09b7a28fe1ee15eb1482
Ian Halstead <iwhalstead10@gmail.com>
2020-04-16 18:07:11 -0600

    Idk why the animations work, but they do

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -4,10 +4,14 @@ const hamburger = document.getElementById('hamburger');
 hamburger.addEventListener('click', (e) => {
 	if (nav.classList.contains('nav-anim')) {
 		nav.classList.remove('nav-anim');
+		//I don't know why this line is required but without it the animation only plays once then never again. go to https://css-tricks.com/restart-css-animation/ for a little more info
+		void nav.offsetWidth;
 		nav.classList.add('nav-anim-hide');
 	}
 	else {
 		nav.classList.remove('nav-anim-hide');
+		//I don't know why this line is required but without it the animation only plays once then never again. go to https://css-tricks.com/restart-css-animation/ for a little more info
+		void nav.offsetWidth;
 		nav.classList.add('nav-anim');
 	}
 
